53bd956b333ee65833d2004c94701909f35112dd
Jan Toebes <jan@toebes.info>
2017-07-26 07:46:12 +0200

    -

---
+++ 27_lognumber.hs
@@ -0,0 +1,10 @@
+import Control.Monad.Writer
+
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+multiWithLog :: Writer [String] Int
+multiWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  return (a*b)
